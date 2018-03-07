ce09d1b744d2ac11fb8050609fc8178d97610551
Zoran Zaric <zz@zoranzaric.de>
2018-03-07 21:34:12 +0000

    Format

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ use chrono::prelude::*;
 
 use csv::StringRecord;
 
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 extern crate loggerv;
 
-#[macro_use] extern crate clap;
-use clap::{Arg, App, SubCommand};
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg, SubCommand};
 
 use influent::create_client;
 use influent::client::{Client, Credentials};
@@ -25,74 +27,73 @@ fn read_weight(path: &Path) -> Result<(), Box<Error>> {
     let client = connect();
 
     let mut rdr = match csv::ReaderBuilder::new()
-                    .has_headers(true)
-                    .delimiter(b',')
-                    .quote(b'"')
-                    .from_path(path) {
-            Ok(rdr) => rdr,
-            Err(e) => { panic!("{}", e); }
-        };
+        .has_headers(true)
+        .delimiter(b',')
+        .quote(b'"')
+        .from_path(path)
+    {
+        Ok(rdr) => rdr,
+        Err(e) => {
+            panic!("{}", e);
+        }
+    };
     for result in rdr.records() {
         let record = result?;
 
         let m = convert_weight_record_to_measurement(record);
         debug!("{:?}", m);
         match client.write_one(m, None) {
-            Ok(_) => { },
-            Err(e) => { error!("{:?}", e) },
+            Ok(_) => {}
+            Err(e) => error!("{:?}", e),
         }
-
     }
     Ok(())
 }
 
 fn convert_weight_record_to_measurement(record: StringRecord) -> Measurement<'static> {
-        debug!("{:?}", record);
+    debug!("{:?}", record);
 
-        let weight: Option<f64> = match record.get(1) {
-            Option::None => None,
-            Option::Some(s) =>
-                match s.parse() {
-                    Result::Err(_) => None,
-                    Result::Ok(weight) => Some(weight),
-                }
-        };
-        let fat: Option<f64> = match record.get(2) {
-            Option::None => None,
-            Option::Some(s) =>
-                match s.parse() {
-                    Result::Err(_) => None,
-                    Result::Ok(fat) => Some(fat),
-                }
-        };
+    let weight: Option<f64> = match record.get(1) {
+        Option::None => None,
+        Option::Some(s) => match s.parse() {
+            Result::Err(_) => None,
+            Result::Ok(weight) => Some(weight),
+        },
+    };
+    let fat: Option<f64> = match record.get(2) {
+        Option::None => None,
+        Option::Some(s) => match s.parse() {
+            Result::Err(_) => None,
+            Result::Ok(fat) => Some(fat),
+        },
+    };
 
+    let local: NaiveDateTime =
+        NaiveDateTime::parse_from_str(record.get(0).unwrap(), "%Y-%m-%d %H:%M:%S").unwrap();
 
-        let local: NaiveDateTime = NaiveDateTime::parse_from_str(record.get(0).unwrap(), "%Y-%m-%d %H:%M:%S").unwrap();
-
-        let mut m = Measurement::new("weight");
-        m.set_timestamp(local.timestamp());
-        m.add_tag("name", "Zoran");
-        match weight {
-            Some(weight) => { 
-                m.add_field("weight", Value::Float(weight));
-            },
-            None => { },
-        };
-        match fat {
-            Some(fat) => { 
-                m.add_field("fat", Value::Float(fat));
-            },
-            None => { },
-        };
-        return m;
+    let mut m = Measurement::new("weight");
+    m.set_timestamp(local.timestamp());
+    m.add_tag("name", "Zoran");
+    match weight {
+        Some(weight) => {
+            m.add_field("weight", Value::Float(weight));
+        }
+        None => {}
+    };
+    match fat {
+        Some(fat) => {
+            m.add_field("fat", Value::Float(fat));
+        }
+        None => {}
+    };
+    return m;
 }
-
 
 fn connect<'a>() -> HttpClient<'a> {
     let credentials = Credentials {
         username: "root",
         password: "root",
-        database: "health"
+        database: "health",
     };
     let hosts = vec!["http://localhost:8086"];
     let client = create_client(credentials, hosts);
@@ -101,24 +102,27 @@ fn connect<'a>() -> HttpClient<'a> {
 
 fn main() {
     let matches = App::new("nokiahealth")
-                      .version(crate_version!())
-                      .author(crate_authors!())
-                      .arg(Arg::with_name("v")
-                          .short("v")
-                          .help("Sets the level of verbosity")
-                          .multiple(true)
-                          )
-                      .subcommand(SubCommand::with_name("weight")
-                          .about("Imports weights from a Nokia Health CSV file to an InfluxDB")
-                          .version(crate_version!())
-                          .author(crate_authors!())
-                          .arg(Arg::with_name("INPUT")
-                              .help("Sets the input file to use")
-                              .required(true)
-                              .index(1)
-                              )
-                          )
-                      .get_matches();
+        .version(crate_version!())
+        .author(crate_authors!())
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .help("Sets the level of verbosity")
+                .multiple(true),
+        )
+        .subcommand(
+            SubCommand::with_name("weight")
+                .about("Imports weights from a Nokia Health CSV file to an InfluxDB")
+                .version(crate_version!())
+                .author(crate_authors!())
+                .arg(
+                    Arg::with_name("INPUT")
+                        .help("Sets the input file to use")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .get_matches();
     loggerv::Logger::new()
         .verbosity(matches.occurrences_of("v"))
         .level(true)
@@ -137,13 +141,12 @@ fn main() {
                 println!("error running read_weight: {}", err);
                 process::exit(1);
             }
-        },
+        }
         None => {
             eprintln!("No subcommand");
-        },
+        }
         _ => {
             eprintln!("Unknown subcommand");
-        },
-
+        }
     }
 }
